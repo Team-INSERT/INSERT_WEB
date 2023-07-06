8e51deeeccf56d80080561e7608d5e3d38e90c1f
Ubinquitous <ubinquitous1@gmail.com>
2023-07-06 22:48:12 +0900

    chore(home): 임의로 header import

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Header from "../../components/Header";
 
 function Home() {
-  return <div />;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 }
 
 export default Home;
