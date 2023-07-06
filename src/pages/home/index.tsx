1bd452f2532b2580780c8fa789f79f60f80234be
Ubinquitous <ubinquitous1@gmail.com>
2023-07-06 22:57:17 +0900

    chore(home): 임의로 footer, 의미없는 내용 div 추가

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import Header from "../../components/Header";
+import Footer from "../../components/Footer";
 
 function Home() {
   return (
     <div>
       <Header />
+      <div style={{ height: "70vh" }} />
+      <Footer />
     </div>
   );
 }
