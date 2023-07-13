984c233d8f3d7ef190f94a0457b0b54be5441c13
youngeunkim <pkhj286@gmail.com>
2023-07-13 11:42:16 +0900

    chore: HomePage 컴포넌트 퍼블리싱, 스타일 코드 작성

---
+++ src/pages/home/layouts/HomePage/index.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import Logo from "../../../../assets/logo.svg";
+import * as S from "./style";
+
+function HomePage() {
+  const { team, code, watch, commit, time } = {
+    team: "Team. INSERT",
+    code: "168,257+",
+    watch: "560,257+",
+    commit: "2,358+",
+    time: "2,650+",
+  };
+
+  return (
+    <S.HomePageLayOut>
+      <S.HomePageHeader>
+        <S.HomePageTitle>{team}</S.HomePageTitle>
+        <S.HomePageSubTitle>
+          안녕하시와요일단멋진말넣기전에...
+        </S.HomePageSubTitle>
+        <S.HomePageLogo src={Logo} />
+        <S.HomePageHr />
+      </S.HomePageHeader>
+      <S.HomePageMiddle>
+        <S.HomePageCodeSection>
+          <S.HomePageCodeText>작성한 코드 라인</S.HomePageCodeText>
+          <S.HomePageCode>{code}</S.HomePageCode>
+        </S.HomePageCodeSection>
+        <S.HomePageWatchSection>
+          <S.HomePageWatchText>웹 페이지 조회수</S.HomePageWatchText>
+          <S.HomePageWatch>{watch}</S.HomePageWatch>
+        </S.HomePageWatchSection>
+      </S.HomePageMiddle>
+      <S.HomePageMiddle2>
+        <S.HomePageCommitSection>
+          <S.HomePageCommitText>총 커밋 횟수</S.HomePageCommitText>
+          <S.HomePageCommit>{commit}</S.HomePageCommit>
+        </S.HomePageCommitSection>
+        <S.HomePageTimeSection>
+          <S.HomePageTimeText>함께 한 시간</S.HomePageTimeText>
+          <S.HomePageTime>{time}</S.HomePageTime>
+        </S.HomePageTimeSection>
+      </S.HomePageMiddle2>
+      <S.HomePageFooter>2023년 6월 기준</S.HomePageFooter>
+    </S.HomePageLayOut>
+  );
+}
+
+export default HomePage;
---
+++ src/pages/home/layouts/HomePage/style.ts
@@ -0,0 +1,140 @@
+import styled from "styled-components";
+import { font } from "../../../../styles/font";
+import color from "../../../../styles/color";
+
+export const HomePageLayOut = styled.div`
+  color: ${color.white};
+  display: flex;
+  flex-direction: column;
+  background-color: ${color.white};
+`;
+
+export const HomePageLogo = styled.img`
+  width: 101.464px;
+  height: 94.953px;
+  margin: 4% 0 0 80%;
+  position: absolute;
+`;
+
+export const HomePageHeader = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const HomePageTitle = styled.h1`
+  color: ${color.black};
+  font-size: 72px;
+  margin: 3% 0 0 10%;
+`;
+
+export const HomePageSubTitle = styled.p`
+  display: flex;
+  width: 800px;
+  color: ${color.black};
+  margin: 0 0 0 10%;
+  font-weight: 700;
+  font-size: 28px;
+`;
+
+export const HomePageHr = styled.div`
+  display: flex;
+  width: 1230px;
+  height: 2px;
+  background-color: #d9d9d9;
+  margin: 1% 0 0 10%;
+`;
+
+export const HomePageCodeSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 5% 0 0 10%;
+`;
+
+export const HomePageCodeText = styled.p`
+  color: ${color.text};
+  display: flex;
+  font-size: 22px;
+  font-weight: 600;
+`;
+
+export const HomePageCode = styled.p`
+  color: ${color.black};
+  font-size: 54px;
+  font-weight: 700;
+  display: flex;
+`;
+
+export const HomePageWatchSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 5% 0 0 10%;
+`;
+
+export const HomePageWatchText = styled.p`
+  color: ${color.text};
+  display: flex;
+  font-size: 22px;
+  font-weight: 600;
+`;
+
+export const HomePageWatch = styled.p`
+  color: ${color.black};
+  font-size: 54px;
+  font-weight: 700;
+  display: flex;
+`;
+
+export const HomePageMiddle = styled.div`
+  display: flex;
+`;
+
+export const HomePageMiddle2 = styled.div`
+  display: flex;
+`;
+
+export const HomePageCommitSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 7% 0 0 10%;
+`;
+
+export const HomePageCommitText = styled.p`
+  color: ${color.text};
+  display: flex;
+  font-size: 22px;
+  font-weight: 600;
+`;
+
+export const HomePageCommit = styled.p`
+  color: ${color.black};
+  font-size: 54px;
+  font-weight: 700;
+  display: flex;
+`;
+
+export const HomePageTimeSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 7% 0 0 14%;
+`;
+
+export const HomePageTime = styled.p`
+  color: ${color.black};
+  font-size: 54px;
+  font-weight: 700;
+  display: flex;
+`;
+
+export const HomePageTimeText = styled.p`
+  color: ${color.text};
+  display: flex;
+  font-size: 22px;
+  font-weight: 600;
+`;
+
+export const HomePageFooter = styled.p`
+  color: ${color.text};
+  font-size: 15px;
+  font-weight: 700px;
+  margin: 3% 0 2.5% 10%;
+`;
