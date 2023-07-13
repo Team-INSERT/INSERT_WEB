e9e78b28a4a8128207b9fdc5e0db364e70279194
youngeunkim <pkhj286@gmail.com>
2023-07-13 11:40:31 +0900

    chore: 의미없는 div 없애고 홈 컴포넌트 임포트

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
+import HomePage from "./layouts/HomePage";
 
 function Home() {
   return (
     <div>
       <Header />
-      <div style={{ height: "70vh" }} />
+      <HomePage />
       <Footer />
     </div>
   );
