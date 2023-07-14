5fd4a9a7aa21c76a00bcd9ca8eee5015e35b8425
kimsiyeon0223 <Ksyeon0223@gmail.com>
2023-07-14 23:58:12 +0900

    chore : History 컴포넌트 임포트

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "src/components/Header";
 import Footer from "src/components/Footer";
+import History from "src/pages/home/layouts/History";
 import HomePage from "src/pages/home/layouts/HomePage";
 
 function Home() {
@@ -8,6 +9,7 @@ function Home() {
     <div>
       <Header />
       <HomePage />
+      <History />
       <Footer />
     </div>
   );
