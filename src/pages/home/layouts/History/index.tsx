fc952efd0d2db9313dad96dea6d178d5b83a9377
kimsiyeon0223 <Ksyeon0223@gmail.com>
2023-07-16 22:31:33 +0900

    chore : 이미지경로 수정, 소개글 추가

--- src/pages/home/layouts/History/index.tsx
+++ src/pages/home/layouts/History/index.tsx
@@ -1,11 +1,12 @@
 import React from "react";
-import School from "src/assets/school.webp";
 import * as S from "./style";
 
 function HistoryPage() {
-  const { title, introduce1 } = {
+  const { title, introduce1, introduce2 } = {
     title: "학교와 함께 역사를 써내려갑니다.",
-    introduce1: "대충 소개하는 글",
+    introduce1: "INSERT는 2023년 3월에 만들어진 부산 소마고 전공동아리 입니다.",
+    introduce2:
+      "저희는 학교 플랫폼 개발 동아리로 학교 커뮤니티를 발전시키는 프로젝트를 진행합니다.",
   };
 
   return (
@@ -13,8 +14,11 @@ function HistoryPage() {
       <S.HistoryContainer>
         <S.HistoryText>
           <S.HistoryTitle>{title}</S.HistoryTitle>
-          <S.HistoryIntroduce>{introduce1}</S.HistoryIntroduce>
-          <S.HistoryImg src={School} />
+          <S.HistoryIntroduce>
+            <S.HistoryIntroduce1>{introduce1}</S.HistoryIntroduce1>
+            <S.HistoryIntroduce2>{introduce2}</S.HistoryIntroduce2>
+          </S.HistoryIntroduce>
+          <S.HistoryImg src="imgs/school.webp" />
         </S.HistoryText>
       </S.HistoryContainer>
     </S.HistoryLayout>
