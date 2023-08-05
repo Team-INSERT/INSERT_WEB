cf8d5e82ff24cbf46dd9145fb4c0ad91b022b24b
kimsiyeon0223 <Ksyeon0223@gmail.com>
2023-08-05 23:12:22 +0900

    refactor : 카멜케이스

--- src/pages/home/layouts/Member/index.tsx
+++ src/pages/home/layouts/Member/index.tsx
@@ -2,12 +2,11 @@ import React, { useState } from "react";
 import * as S from "./style";
 
 function Member() {
-  const IntroMember = [
+  const introMember = [
     {
       memberId: 1,
       name: "정동겸",
-      generation: 1,
-      subGeneration: "st",
+      generation: "1st",
       tech: "Frontend Developer",
       github_url: "https://github.com/jeongdonggyeom",
       github_img_url: "https://avatars.githubusercontent.com/u/80656788?v=4",
@@ -15,8 +14,7 @@ function Member() {
     {
       memberId: 2,
       name: "정승민",
-      generation: 1,
-      subGeneration: "st",
+      generation: "1st",
       tech: "Frontend Developer",
       github_url: "https://github.com/jsm8109jsm",
       github_img_url: "https://avatars.githubusercontent.com/u/105054567?v=4",
@@ -24,8 +22,7 @@ function Member() {
     {
       memberId: 3,
       name: "홍지민",
-      generation: 1,
-      subGeneration: "st",
+      generation: "1st",
       tech: "Frontend Developer",
       github_url: "https://github.com/J1min",
       github_img_url: "https://avatars.githubusercontent.com/u/80014454?v=4",
@@ -33,8 +30,7 @@ function Member() {
     {
       memberId: 4,
       name: "김영민",
-      generation: 1,
-      subGeneration: "st",
+      generation: "1st",
       tech: "Backend Developer",
       github_url: "https://github.com/min050410",
       github_img_url: "https://avatars.githubusercontent.com/u/45661217?v=4",
@@ -42,8 +38,7 @@ function Member() {
     {
       memberId: 5,
       name: "이현준",
-      generation: 1,
-      subGeneration: "st",
+      generation: "2rd",
       tech: "Backend Developer",
       github_url: "https://github.com/leehj050211",
       github_img_url: "https://avatars.githubusercontent.com/u/80656849?v=4",
@@ -51,8 +46,7 @@ function Member() {
     {
       memberId: 6,
       name: "권세원",
-      generation: 2,
-      subGeneration: "nd",
+      generation: "2nd",
       tech: "Frontend Developer",
       github_url: "https://github.com/5ewon",
       github_img_url: "https://avatars.githubusercontent.com/u/101105694?v=4",
@@ -60,17 +54,15 @@ function Member() {
     {
       memberId: 7,
       name: "박우빈",
-      generation: 2,
+      generation: "2nd",
       tech: "Frontend Developer",
-      subGeneration: "nd",
       github_url: "https://github.com/Ubinquitous",
       github_img_url: "https://avatars.githubusercontent.com/u/102154880?v=4",
     },
     {
       memberId: 8,
       name: "김호현",
-      generation: 2,
-      subGeneration: "nd",
+      generation: "2nd",
       tech: "Backend Developer",
       github_url: "https://github.com/qlido",
       github_img_url: "https://avatars.githubusercontent.com/u/67864410?v=4",
@@ -78,8 +70,7 @@ function Member() {
     {
       memberId: 9,
       name: "이창보",
-      generation: 2,
-      subGeneration: "nd",
+      generation: "2nd",
       tech: "Backend Developer",
       github_url: "https://github.com/jacobhboy",
       github_img_url: "https://avatars.githubusercontent.com/u/101192100?v=4",
@@ -87,8 +78,7 @@ function Member() {
     {
       memberId: 10,
       name: "김시연",
-      generation: 3,
-      subGeneration: "rd",
+      generation: "3rd",
       tech: "Frontend Developer",
       github_url: "https://github.com/kimsiyeon0223",
       github_img_url: "https://avatars.githubusercontent.com/u/128461588?v=4",
@@ -96,8 +86,7 @@ function Member() {
     {
       memberId: 11,
       name: "김영은",
-      generation: 3,
-      subGeneration: "rd",
+      generation: "3rd",
       tech: "Frontend Developer",
       github_url: "https://github.com/zeroeuni",
       github_img_url: "https://avatars.githubusercontent.com/u/128370837?v=4",
@@ -105,8 +94,7 @@ function Member() {
     {
       memberId: 12,
       name: "전영현",
-      generation: 3,
-      subGeneration: "rd",
+      generation: "3rd",
       tech: "Frontend Developer",
       github_url: "https://github.com/jyh071116",
       github_img_url: "https://avatars.githubusercontent.com/u/128202921?v=4",
@@ -114,8 +102,7 @@ function Member() {
     {
       memberId: 13,
       name: "강지원",
-      generation: 3,
-      subGeneration: "rd",
+      generation: "3rd",
       tech: "Backend Developer",
       github_url: "https://github.com/J1W0N-1209",
       github_img_url: "https://avatars.githubusercontent.com/u/113921639?v=4",
@@ -123,8 +110,7 @@ function Member() {
     {
       memberId: 14,
       name: "오윤찬",
-      generation: 3,
-      subGeneration: "rd",
+      generation: "3rd",
       tech: "Backend Developer",
       github_url: "https://github.com/YunChan-Oh",
       github_img_url: "https://avatars.githubusercontent.com/u/91408117?v=4",
@@ -132,15 +118,14 @@ function Member() {
     {
       memberId: 15,
       name: "전유진",
-      generation: 3,
-      subGeneration: "rd",
+      generation: "3rd",
       tech: "Backend Developer",
       github_url: "https://github.com/jyj1289",
       github_img_url: "https://avatars.githubusercontent.com/u/121763556?v=4",
     },
   ];
 
-  const [currentGeneration, setCurrentGeneration] = useState(1);
+  const [currentGeneration, setCurrentGeneration] = useState("1st");
 
   return (
     <S.MemberLayout>
@@ -151,25 +136,25 @@ function Member() {
           <S.MemberGrade>
             <S.GradeTitle>Grade</S.GradeTitle>
             <S.GradeButton>
-              <S.GradeButton1 onClick={() => setCurrentGeneration(1)}>
+              <S.GradeButton1 onClick={() => setCurrentGeneration("1st")}>
                 1st
               </S.GradeButton1>
-              <S.GradeButton2 onClick={() => setCurrentGeneration(2)}>
+              <S.GradeButton2 onClick={() => setCurrentGeneration("2nd")}>
                 2nd
               </S.GradeButton2>
-              <S.GradeButton3 onClick={() => setCurrentGeneration(3)}>
+              <S.GradeButton3 onClick={() => setCurrentGeneration("3rd")}>
                 3rd
               </S.GradeButton3>
-              <S.GradeButton4 onClick={() => setCurrentGeneration(4)}>
+              <S.GradeButton4 onClick={() => setCurrentGeneration("4th")}>
                 4th
               </S.GradeButton4>
-              <S.GradeButton5 onClick={() => setCurrentGeneration(5)}>
+              <S.GradeButton5 onClick={() => setCurrentGeneration("5th")}>
                 5th
               </S.GradeButton5>
             </S.GradeButton>
           </S.MemberGrade>
           <S.Member>
-            {IntroMember.map((member, memberId) => (
+            {introMember.map((member, memberId) => (
               <S.MemberMiddle>
                 {member.generation === currentGeneration && (
                   <S.MemberProfile key={memberId}>
@@ -183,8 +168,7 @@ function Member() {
                     <S.Information>
                       <S.Name>{member.name}</S.Name>
                       <S.Role>
-                        {member.generation}
-                        {member.subGeneration} | {member.tech}
+                        {member.generation} | {member.tech}
                       </S.Role>
                     </S.Information>
                   </S.MemberProfile>
