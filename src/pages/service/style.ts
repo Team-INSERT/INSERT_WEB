57227583d07258bd9b2a51bd34f06a0a05bd31b9
youngeunkim <pkhj286@gmail.com>
2023-08-06 11:54:29 +0900

    refactor : %로 단위 변경

--- src/pages/service/style.ts
+++ src/pages/service/style.ts
@@ -11,47 +11,45 @@ export const Container = styled.div`
 
 export const Title = styled.div`
   ${font.D3}
-  letter-spacing: -2px;
+  letter-spacing: -0.5%;
   color: ${color.black};
-  margin-bottom: 2.5rem;
+  margin-bottom: 1%;
 `;
 
 export const SubTitle = styled.div`
   ${font.H4}
   color: ${color.text};
-  width: 30.25rem;
-  margin-bottom: 3rem;
+  width: 60%;
+  margin-bottom: 1.5%;
 `;
 
 export const ProjectLayout = styled.div`
   display: flex;
   border-bottom: 2px solid #d9d9d9;
-  padding: 3rem 0;
+  padding: 3% 0;
 `;
 
 export const ProjectLogo = styled.img<{ direction: string }>`
-  width: 35rem;
+  width: 80%;
   ${(props) =>
     props.direction === "left"
       ? css`
-          margin-right: 2rem;
+          margin-right: 0;
         `
       : css`
-          margin-left: 17.5rem;
+          margin-left: 55%;
         `}
 `;
 
 export const ProjectTitle = styled.div`
   ${font.D3}
-  margin-bottom: 0.5rem;
+  margin-bottom: 1%;
 `;
 
 export const ProjectSubTitle = styled.div`
   ${font.H5}
   color: ${color.text};
-  &:first-child {
-    margin-bottom: 7.5rem;
-  }
+  margin-bottom: 5rem;
 `;
 
 export const ProjectContentsLayout = styled.div`
@@ -60,8 +58,13 @@ export const ProjectContentsLayout = styled.div`
 `;
 
 export const Contributors = styled.div`
+  ${font.H5}
+  color: ${color.text};
+`;
+
+export const ContributorBox = styled.div`
   display: flex;
-  margin-top: 0.5rem;
+  margin-top: 1%;
 `;
 
 export const Contributor = styled.img`
