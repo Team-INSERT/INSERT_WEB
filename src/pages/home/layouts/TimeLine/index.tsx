e9915344af1105eb9c90039022e293f882ed66da
youngeunkim <pkhj286@gmail.com>
2023-08-26 11:35:16 +0900

    Refactor : JSON 사용해서 타임라인

--- src/pages/home/layouts/TimeLine/index.tsx
+++ src/pages/home/layouts/TimeLine/index.tsx
@@ -1,13 +1,12 @@
 import { useState } from "react";
 import * as S from "./style";
+import dates from "./data.json";
 
 function TimeLine() {
-  const dates = ["2023", "2024", "2025", "2026", "2027"];
-
-  const [clickedDate, setClickedDate] = useState("2023");
+  const [clickedDate, setClickedDate] = useState<string | null>(null);
 
   const handleDateClick = (date: string) => {
-    setClickedDate(date);
+    setClickedDate(date === clickedDate ? null : date);
   };
 
   return (
@@ -16,51 +15,31 @@ function TimeLine() {
       <S.SubTitle>INSERT가 함께한 멋진 시간들을 소개합니다.</S.SubTitle>
       <S.TimeLineBox>
         <S.TimeLine>
-          {dates.map((date) => (
-            <S.TimeLineItem key={date}>
-              <S.TimeLineDate onClick={() => handleDateClick(date)}>
-                {date}
+          {dates.map((dateData) => (
+            <S.TimeLineItem key={dateData.year}>
+              <S.TimeLineDate
+                onClick={() => handleDateClick(dateData.year)}
+                isActive={clickedDate === dateData.year}
+              >
+                {dateData.year}
               </S.TimeLineDate>
               <S.TimeLineO />
             </S.TimeLineItem>
           ))}
         </S.TimeLine>
-        {clickedDate && (
-          <S.TimeLineText>
-            {clickedDate === "2023" && (
-              <S.TimeLineYear>
-                <S.TimeLineContentDate>2023.03</S.TimeLineContentDate>
-                <S.TimeLineContent>
-                  3월의 어느 날... 인서트 결성
-                </S.TimeLineContent>
-              </S.TimeLineYear>
-            )}
-            {clickedDate === "2024" && (
-              <S.TimeLineYear>
-                <S.TimeLineContentDate>2024</S.TimeLineContentDate>
-                <S.TimeLineContent>텅~</S.TimeLineContent>
-              </S.TimeLineYear>
-            )}
-            {clickedDate === "2025" && (
-              <S.TimeLineYear>
-                <S.TimeLineContentDate>2025</S.TimeLineContentDate>
-                <S.TimeLineContent>텅~</S.TimeLineContent>
-              </S.TimeLineYear>
-            )}
-            {clickedDate === "2026" && (
-              <S.TimeLineYear>
-                <S.TimeLineContentDate>2026</S.TimeLineContentDate>
-                <S.TimeLineContent>텅~</S.TimeLineContent>
-              </S.TimeLineYear>
-            )}
-            {clickedDate === "2027" && (
-              <S.TimeLineYear>
-                <S.TimeLineContentDate>2027</S.TimeLineContentDate>
-                <S.TimeLineContent>텅~</S.TimeLineContent>
-              </S.TimeLineYear>
-            )}
-          </S.TimeLineText>
-        )}
+        <S.TimeLineText>
+          {dates.map(
+            (dateData) =>
+              clickedDate === dateData.year && (
+                <S.TimeLineYear key={dateData.year}>
+                  <S.TimeLineContentDate>{dateData.year}</S.TimeLineContentDate>
+                  {dateData.contents.map((content, index) => (
+                    <S.TimeLineContent key={index}>{content}</S.TimeLineContent>
+                  ))}
+                </S.TimeLineYear>
+              ),
+          )}
+        </S.TimeLineText>
       </S.TimeLineBox>
     </S.Container>
   );
