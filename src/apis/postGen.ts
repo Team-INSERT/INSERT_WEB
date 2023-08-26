ce73f6e438489ae1d37217ab2d9fa9f2c2e19d60
youngeunkim <pkhj286@gmail.com>
2023-08-26 14:57:38 +0900

    chore : custom axios

--- src/apis/postGen.ts
+++ src/apis/postGen.ts
@@ -1,9 +1,9 @@
-import axios from "axios";
+import { customAxios } from "lib/customAxios";
 
 export const PostGen = async (name: string) => {
   const newGen = new Date().getFullYear() - 2021;
 
-  await axios.post(`${process.env.REACT_APP_BASEURL}/api/member/generation`, {
+  await customAxios.post("/api/member/generation", {
     accessCode: process.env.REACT_APP_ACCESS_TOKEN,
     githubId: name,
     generation: newGen,
